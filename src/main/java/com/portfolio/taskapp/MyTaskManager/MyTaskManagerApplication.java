f0be4306b70d1ce5d05ab4db90414c7d1f7d51ff
Lika <wvouov.22.vouovw@gmail.com>
2025-07-27 17:24:16 +0900

    API仕様書の設定を追加(正常レスポンスのみ)

--- src/main/java/com/portfolio/taskapp/MyTaskManager/MyTaskManagerApplication.java
+++ src/main/java/com/portfolio/taskapp/MyTaskManager/MyTaskManagerApplication.java
@@ -1,13 +1,19 @@
 package com.portfolio.taskapp.MyTaskManager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+    title = "タスク管理システム",
+    description = "個人タスクを管理するためのシステムです。"
+))
 @SpringBootApplication
 public class MyTaskManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyTaskManagerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MyTaskManagerApplication.class, args);
+  }
 
 }
--- src/main/java/com/portfolio/taskapp/MyTaskManager/controller/TaskController.java
+++ src/main/java/com/portfolio/taskapp/MyTaskManager/controller/TaskController.java
@@ -2,6 +2,12 @@ package com.portfolio.taskapp.MyTaskManager.controller;
 
 import com.portfolio.taskapp.MyTaskManager.domain.entity.Project;
 import com.portfolio.taskapp.MyTaskManager.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +24,25 @@ public class TaskController {
     this.service = service;
   }
 
+  @Operation(
+      summary = "ユーザープロジェクトの一覧取得",
+      description = "ユーザーのid情報に紐づくプロジェクト情報の一覧を取得します",
+      parameters = {
+          @Parameter(name = "userPublicId",
+              required = true,
+              description = "ユーザーの公開ID（UUID）(ログイン手法の方針により変更する可能性があります。)",
+              schema = @Schema(
+                  type = "string",
+                  format = "uuid",
+                  example = "5998fd5d-a2cd-11ef-b71f-6845f15f510c"
+              )
+          )},
+      responses = {
+          @ApiResponse(
+              content = @Content(mediaType = "application/json",
+                  array = @ArraySchema(schema = @Schema(implementation = Project.class)))
+          )}
+  )
   @GetMapping("/my-project")
   public List<Project> getProjectList(@RequestParam String userPublicId) {
     // TODO: 本番ではトークンから取得するように変更
