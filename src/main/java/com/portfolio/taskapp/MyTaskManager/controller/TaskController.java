cde3a4b5fc258c2c5a5c970a12c5e93464b53f85
Lika <wvouov.22.vouovw@gmail.com>
2025-07-28 15:49:21 +0900

    プロジェクトのタスク一覧を取得するAPIを実装

--- src/main/java/com/portfolio/taskapp/MyTaskManager/controller/TaskController.java
+++ src/main/java/com/portfolio/taskapp/MyTaskManager/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.portfolio.taskapp.MyTaskManager.controller;
 
 import com.portfolio.taskapp.MyTaskManager.domain.entity.Project;
+import com.portfolio.taskapp.MyTaskManager.domain.entity.Task;
 import com.portfolio.taskapp.MyTaskManager.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +48,32 @@ public class TaskController {
   public List<Project> getProjectList(@RequestParam String userPublicId) {
     // TODO: 本番ではトークンから取得するように変更
     return service.getUserProjects(userPublicId);
+  }
+
+  @Operation(
+      summary = "プロジェクトのタスク一覧取得",
+      description = "プロジェクトのid情報に紐づく全タスク情報の一覧を取得します。(サブタスクも含む）",
+      parameters = {
+          @Parameter(name = "projectPublicId",
+              required = true,
+              description = "プロジェクトの公開ID（UUID）",
+              schema = @Schema(
+                  type = "string",
+                  format = "uuid",
+                  example = "5998fd5d-a2cd-11ef-b71f-6845f15f510c"
+              )
+          )},
+      responses = {
+          @ApiResponse(
+              responseCode = "200",
+              description = "リクエストが正常に処理された場合",
+              content = @Content(mediaType = "application/json",
+                  array = @ArraySchema(schema = @Schema(implementation = Task.class)))
+          )}
+  )
+  @GetMapping("/projects/{projectPublicId}/tasks")
+  public List<Task> getTaskListByProjectPublicId(@PathVariable String projectPublicId) {
+    return service.getTasksByProjectPublicId(projectPublicId);
   }
 
 }
