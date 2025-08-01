3aac857c99ed7a3ec98ecf7713cf396f4ed8214e
Lika <wvouov.22.vouovw@gmail.com>
2025-08-02 00:26:20 +0900

    Security設定の変更

--- src/main/java/com/portfolio/taskapp/MyTaskManager/auth/config/SecurityConfig.java
+++ src/main/java/com/portfolio/taskapp/MyTaskManager/auth/config/SecurityConfig.java
@@ -2,13 +2,12 @@ package com.portfolio.taskapp.MyTaskManager.auth.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import com.portfolio.taskapp.MyTaskManager.auth.service.UserAccountDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,13 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-  private final UserAccountDetailsService accountDetailsService;
-
-  @Autowired
-  public SecurityConfig(UserAccountDetailsService accountDetailsService) {
-    this.accountDetailsService = accountDetailsService;
-  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -32,6 +24,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/login", "/css/**", "/js/**").permitAll()
             .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
