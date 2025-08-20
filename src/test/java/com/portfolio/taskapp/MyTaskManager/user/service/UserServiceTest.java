1f5195161ab6ce351729f066a02bebfc4f5001d9
Lika <wvouov.22.vouovw@gmail.com>
2025-08-20 22:06:34 +0900

    UserServiceのテスト実装内容の見直しと修正

--- src/test/java/com/portfolio/taskapp/MyTaskManager/user/service/UserServiceTest.java
+++ src/test/java/com/portfolio/taskapp/MyTaskManager/user/service/UserServiceTest.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -41,14 +40,18 @@ class UserServiceTest {
 
   private static final String PUBLIC_ID = "00000000-0000-0000-0000-000000000000";
   private static final String EMAIL = "user@example.com";
+  private static final String NEW_EMAIL = "new@example.com";
   private static final String PASSWORD_RAW = "rawPassword";
   private static final String PASSWORD_HASHED = "hashedPassword";
+  private static final String NEW_PASSWORD_RAW = "newRawPassword";
+  private static final String NEW_PASSWORD_HASHED = "newHashedPassword";
 
   @BeforeEach
   void setUp() {
     sut = new UserService(repository, passwordEncoder, mapper);
   }
 
+  // アカウント情報取得：正常系
   @Test
   void アカウント情報取得時に適切なrepositoryとmapperが呼び出されていること() {
     UserAccount account = new UserAccount();
@@ -61,27 +64,27 @@ class UserServiceTest {
     verify(mapper).toUserAccountResponse(account);
   }
 
+  // アカウント登録処理：正常系
   @Test
   void アカウント登録時に適切にrepositoryとencoderが呼び出されていること()
       throws NotUniqueException {
-    AccountRegisterRequest request = new AccountRegisterRequest(null, EMAIL,
-        PASSWORD_RAW);
+    AccountRegisterRequest request = new AccountRegisterRequest(null, EMAIL, PASSWORD_RAW);
     UserAccount registerAccount = new UserAccount();
 
     when(repository.existsByEmail(EMAIL)).thenReturn(false);
     when(passwordEncoder.encode(PASSWORD_RAW)).thenReturn(PASSWORD_HASHED);
-    when(mapper.CreateRequestToUserAccount(eq(request), any(String.class),
-        eq(PASSWORD_HASHED)))
+    when(mapper.CreateRequestToUserAccount(eq(request), any(String.class), eq(PASSWORD_HASHED)))
         .thenReturn(registerAccount);
 
     sut.registerUser(request);
 
     verify(repository).existsByEmail(EMAIL);
     verify(passwordEncoder).encode(PASSWORD_RAW);
-    verify(mapper).CreateRequestToUserAccount(eq(request), any(), eq(PASSWORD_HASHED));
+    verify(mapper).CreateRequestToUserAccount(eq(request), any(String.class), eq(PASSWORD_HASHED));
     verify(repository).registerUserAccount(registerAccount);
   }
 
+  // アカウント登録処理：異常系(400 email重複チェック抵触)
   @Test
   void アカウント登録時のemail重複チェックがTRUEの場合に重複例外がThrowされ以降の処理が実行されないこと() {
     AccountRegisterRequest request = new AccountRegisterRequest(null, EMAIL, null);
@@ -95,28 +98,28 @@ class UserServiceTest {
     verify(passwordEncoder, never()).encode(any());
   }
 
+  // アカウント更新処理：正常系
   @Test
   void アカウント情報更新において適切なmapperとrepositoryが呼び出され認証情報も更新されていること()
       throws NotUniqueException, InvalidPasswordChangeException {
     // 事前準備
-    String newRawPassword = "newRowPassword";
-    String newHashedPassword = "newHashedPassword";
-    AccountUpdateRequest request = new AccountUpdateRequest(null, EMAIL, PASSWORD_RAW,
-        newRawPassword);
+    AccountUpdateRequest request = new AccountUpdateRequest(null, NEW_EMAIL, PASSWORD_RAW,
+        NEW_PASSWORD_RAW);
     UserAccount authAccount = UserAccount.builder()
         .publicId(PUBLIC_ID)
+        .email(EMAIL)
         .password(PASSWORD_HASHED)
         .build();
     UserAccountDetails details = new UserAccountDetails(authAccount);
     UserAccount updateAccount = UserAccount.builder()
-        .email(EMAIL)
-        .password(newHashedPassword)
+        .email(NEW_EMAIL)
+        .password(NEW_PASSWORD_HASHED)
         .build();
 
     when(passwordEncoder.matches(PASSWORD_RAW, PASSWORD_HASHED)).thenReturn(true);
-    when(passwordEncoder.encode(newRawPassword)).thenReturn(newHashedPassword);
-    when(repository.existsByEmail(EMAIL)).thenReturn(false);
-    when(mapper.updateRequestToUserAccount(request, PUBLIC_ID, newHashedPassword))
+    when(passwordEncoder.encode(NEW_PASSWORD_RAW)).thenReturn(NEW_PASSWORD_HASHED);
+    when(repository.existsByEmail(NEW_EMAIL)).thenReturn(false);
+    when(mapper.updateRequestToUserAccount(request, PUBLIC_ID, NEW_PASSWORD_HASHED))
         .thenReturn(updateAccount);
 
     // SecurityContext をクリア
@@ -125,35 +128,16 @@ class UserServiceTest {
     // 実行
     sut.updateAccount(details, request);
 
-    // 検証: 処理工程の確認
+    // 検証
+    // パスワード検証メソッドの間接呼び出し確認
     verify(passwordEncoder).matches(PASSWORD_RAW, PASSWORD_HASHED);
-    verify(passwordEncoder).encode(newRawPassword);
-    verify(repository).existsByEmail(EMAIL);
-    verify(mapper).updateRequestToUserAccount(request, PUBLIC_ID, newHashedPassword);
+    // email重複チェックメソッドの間接呼び出し確認
+    verify(repository).existsByEmail(NEW_EMAIL);
+    verify(mapper).updateRequestToUserAccount(request, PUBLIC_ID, NEW_PASSWORD_HASHED);
     verify(repository).updateAccount(updateAccount);
     verify(mapper).toUserAccountResponse(updateAccount);
-
-    // 検証: updateAuthInfoでの認証情報の更新確認
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    assertThat(authentication).isNotNull();
-    assertThat(authentication.getPrincipal()).isInstanceOf(UserAccountDetails.class);
-
-    UserAccountDetails updatedDetails = (UserAccountDetails) authentication.getPrincipal();
-    assertThat(updatedDetails.getUsername()).isEqualTo(EMAIL);
-    assertThat(updatedDetails.getPassword()).isEqualTo(newHashedPassword);
-  }
-
-
-  @Test
-  void アカウント更新時にemail重複チェックがTRUEの場合に重複例外がThrowされ以降の処理が実行されないこと() {
-    AccountUpdateRequest request = new AccountUpdateRequest(null, EMAIL, null, null);
-    UserAccountDetails details = new UserAccountDetails(new UserAccount());
-
-    when(repository.existsByEmail(EMAIL)).thenReturn(true);
-
-    assertThatThrownBy(() -> sut.updateAccount(details, request))
-        .isInstanceOf(NotUniqueException.class)
-        .hasMessage("このメールアドレスは使用できません");
+    // 認証情報更新メソッドの間接呼び出し確認
+    assertThat(SecurityContextHolder.getContext().getAuthentication()).isNotNull();
   }
 
 }
