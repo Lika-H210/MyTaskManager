f48b0dce495bbfc21e1f71a390177b514e4d6376
Lika <wvouov.22.vouovw@gmail.com>
2025-08-25 23:53:43 +0900

    UserControllerのテストを単体と結合テストに分割・整理・修正

---
+++ src/test/java/com/portfolio/taskapp/MyTaskManager/integration/UserControllerIntegrationTest.java
@@ -0,0 +1,109 @@
+package com.portfolio.taskapp.MyTaskManager.integration;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portfolio.taskapp.MyTaskManager.auth.config.SecurityConfig;
+import com.portfolio.taskapp.MyTaskManager.auth.model.UserAccountDetails;
+import com.portfolio.taskapp.MyTaskManager.domain.entity.UserAccount;
+import com.portfolio.taskapp.MyTaskManager.user.controller.UserController;
+import com.portfolio.taskapp.MyTaskManager.user.model.AccountResponse;
+import com.portfolio.taskapp.MyTaskManager.user.model.AccountUpdateRequest;
+import com.portfolio.taskapp.MyTaskManager.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+@Import(SecurityConfig.class)
+class UserControllerIntegrationTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockitoBean
+  private UserService service;
+
+  private UserAccountDetails userDetails;
+
+  @BeforeEach
+  void setUp() {
+    UserAccount account = UserAccount.builder()
+        .publicId("00000000-0000-0000-0000-000000000000")
+        .build();
+    userDetails = new UserAccountDetails(account);
+  }
+
+  @Test
+  void アカウント情報取得時に適切なServiceが呼び出され200ステータスでJsonレスポンスが返されること()
+      throws Exception {
+    AccountResponse response = new AccountResponse();
+    String expectJson = objectMapper.writeValueAsString(response);
+    when(service.findAccount(userDetails.getAccount().getPublicId())).thenReturn(response);
+
+    mockMvc.perform(get("/users/me")
+            .with(user(userDetails)))
+        .andExpect(status().isOk())
+        .andExpect(content().json(expectJson));
+
+    verify(service).findAccount(userDetails.getAccount().getPublicId());
+  }
+
+  @Test
+  void アカウント情報取得時に認証情報がない場合は302ステータスが返されること()
+      throws Exception {
+    mockMvc.perform(get("/users/me"))
+        .andExpect(status().isFound());
+  }
+
+  @Test
+  void アカウント更新で適切なserviceが呼び出され200ステータスが返されること()
+      throws Exception {
+    AccountUpdateRequest request = new AccountUpdateRequest(
+        "New Name",
+        "user@email.com",
+        "oldPassword",
+        "newPassword"
+    );
+
+    String json = objectMapper.writeValueAsString(request);
+
+    mockMvc.perform(patch("/users/me")
+            .with(user(userDetails))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json))
+        .andExpect(status().isOk());
+
+    verify(service).updateAccount(any(UserAccountDetails.class), any(AccountUpdateRequest.class));
+  }
+
+  @Test
+  void アカウント削除で適切なserviceが実行と認証情報の削除が実行され204ステータスが返されること()
+      throws Exception {
+    mockMvc.perform(delete("/users/me")
+            .with(user(userDetails)))
+        .andExpect(status().isNoContent())
+        // 認証情報がクリアされることを確認
+        .andExpect(unauthenticated());
+
+    verify(service).deleteAccount(userDetails.getAccount().getPublicId());
+  }
+
+}
--- src/test/java/com/portfolio/taskapp/MyTaskManager/user/controller/UserControllerTest.java
+++ src/test/java/com/portfolio/taskapp/MyTaskManager/user/controller/UserControllerTest.java
@@ -3,35 +3,26 @@ package com.portfolio.taskapp.MyTaskManager.user.controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.portfolio.taskapp.MyTaskManager.auth.config.SecurityConfig;
-import com.portfolio.taskapp.MyTaskManager.auth.model.UserAccountDetails;
-import com.portfolio.taskapp.MyTaskManager.domain.entity.UserAccount;
 import com.portfolio.taskapp.MyTaskManager.user.model.AccountRegisterRequest;
-import com.portfolio.taskapp.MyTaskManager.user.model.AccountResponse;
 import com.portfolio.taskapp.MyTaskManager.user.model.AccountUpdateRequest;
 import com.portfolio.taskapp.MyTaskManager.user.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserController.class)
-@Import(SecurityConfig.class)
+@ImportAutoConfiguration(exclude = SecurityAutoConfiguration.class)
 class UserControllerTest {
 
   @Autowired
@@ -42,33 +33,6 @@ class UserControllerTest {
 
   @MockitoBean
   private UserService service;
-
-  private UserAccountDetails userDetails;
-
-  @BeforeEach
-  void setUp() {
-    UserAccount account = UserAccount.builder()
-        .publicId("00000000-0000-0000-0000-000000000000")
-        .build();
-    userDetails = new UserAccountDetails(account);
-  }
-
-  @Test
-  void アカウント情報取得時に適切なServiceが呼び出され200ステータスとJSON形式のレスポンスが返されること()
-      throws Exception {
-    AccountResponse response = new AccountResponse();
-
-    when(service.findAccount(userDetails.getAccount().getPublicId())).thenReturn(response);
-
-    String expectedJson = objectMapper.writeValueAsString(response);
-
-    mockMvc.perform(get("/users/me")
-            .with(user(userDetails)))
-        .andExpect(status().isOk())
-        .andExpect(content().json(expectedJson));
-
-    verify(service).findAccount(userDetails.getAccount().getPublicId());
-  }
 
   @Test
   void アカウント登録で適切なserviceが呼び出され201ステータスかつ成功メッセージが返り値となること()
@@ -101,46 +65,18 @@ class UserControllerTest {
   }
 
   @Test
-  void アカウント更新で適切なserviceが呼び出され200ステータスが返されること()
-      throws Exception {
-    AccountUpdateRequest request = createAccountUpdateRequest("user@email.com");
-
-    String json = objectMapper.writeValueAsString(request);
-
-    mockMvc.perform(patch("/users/me")
-            .with(user(userDetails))
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(json))
-        .andExpect(status().isOk());
-
-    verify(service).updateAccount(any(UserAccountDetails.class), any(AccountUpdateRequest.class));
-  }
-
-  @Test
   void アカウント更新でバリデーションに抵触する場合にレスポンスで400エラーが返されること()
       throws Exception {
-    AccountUpdateRequest request = createAccountUpdateRequest("email");
+    AccountUpdateRequest request = new AccountUpdateRequest(null, "email", null, null);
+
     String json = objectMapper.writeValueAsString(request);
 
     mockMvc.perform(patch("/users/me")
-            .with(user(userDetails))
             .contentType(MediaType.APPLICATION_JSON)
             .content(json))
         .andExpect(status().isBadRequest());
 
     verify(service, never()).updateAccount(any(), any());
-  }
-
-  @Test
-  void アカウント削除で適切なserviceが実行と認証情報の削除が実行され204ステータスが返されること()
-      throws Exception {
-    mockMvc.perform(delete("/users/me")
-            .with(user(userDetails)))
-        .andExpect(status().isNoContent())
-        // 認証情報がクリアされることを確認
-        .andExpect(unauthenticated());
-
-    verify(service).deleteAccount(userDetails.getAccount().getPublicId());
   }
 
   // UserAccountCreateRequest生成(passwordのみ引数で設定)
@@ -153,13 +89,4 @@ class UserControllerTest {
     );
   }
 
-  private static AccountUpdateRequest createAccountUpdateRequest
-      (String email) {
-    return new AccountUpdateRequest(
-        "New Name",
-        email,
-        "oldPassword",
-        "newPassword"
-    );
-  }
 }
