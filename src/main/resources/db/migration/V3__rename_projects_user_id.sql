423299838868bd7b0e392ee0a7e9d52d565c3f5c
Lika <wvouov.22.vouovw@gmail.com>
2025-09-19 17:18:30 +0900

    fix:DB変更に伴いMapper変数名追加修正とDBのインデックス名変更処理追加

--- src/main/resources/db/migration/V3__rename_projects_user_id.sql
+++ src/main/resources/db/migration/V3__rename_projects_user_id.sql
@@ -1,2 +1,5 @@
--- user_account_id カラム追加
+-- カラム名リネーム：user_id → user_account_id
 ALTER TABLE projects RENAME COLUMN user_id TO user_account_id;
+
+-- インデクス名の変更（インデックス削除 → 修正したインデックスを追加）
+ALTER TABLE projects DROP INDEX idx_user_id, ADD INDEX idx_user_account_id (user_account_id);
